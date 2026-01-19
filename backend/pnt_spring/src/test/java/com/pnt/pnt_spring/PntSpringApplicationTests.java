0d7ee520b9dd4e16b2250616661a4a76a3c26a96
권기범 <xtcc02332@naver.com>
2026-01-19 09:54:33 +0900

    chore: SpringBoot 프로젝트 초기화

---
+++ backend/pnt_spring/src/test/java/com/pnt/pnt_spring/PntSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnt.pnt_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PntSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
