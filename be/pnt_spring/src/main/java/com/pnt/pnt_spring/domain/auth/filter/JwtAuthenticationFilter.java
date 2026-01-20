22241a442d06fc32fe75da0f60a0b7900f437d36
민승환 <minccc42710@gmail.com>
2026-01-20 15:15:22 +0900

    feat : JWT Provider 로직 구현, Spring Security Filter Chain 설정

--- be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/auth/filter/JwtAuthenticationFilter.java
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/auth/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,48 @@
 package com.pnt.pnt_spring.domain.auth.filter;
 
-public class JwtAuthenticationFilter {
+import com.pnt.pnt_spring.domain.auth.jwt.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = resolveToken(request);
+
+        // 토큰 유효성 검사
+        // 정상 토큰이면 SecurityContext 에 저장
+        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // Request Header에서 토큰 정보를 꺼내오기
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
