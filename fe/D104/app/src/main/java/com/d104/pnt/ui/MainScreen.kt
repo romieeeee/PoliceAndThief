83e99ca5440896c87d6420d2e25012a22b8b60bc
winter3671 <wjdgns9805@naver.com>
2026-01-22 15:14:13 +0900

    [FE: feat] 뉴스 생성중 로딩화면 구현
    - 방송 대기중 아나운서 gif 생성 및 추가

--- fe/D104/app/src/main/java/com/d104/pnt/ui/MainScreen.kt
+++ fe/D104/app/src/main/java/com/d104/pnt/ui/MainScreen.kt
@@ -22,6 +22,7 @@ import com.d104.pnt.navigation.BottomNavItem
 import com.d104.pnt.navigation.Routes
 import com.d104.pnt.ui.chatroomlist.ChatRoomListScreen
 import com.d104.pnt.ui.game.create.GameCreateScreen
+import com.d104.pnt.ui.game.result.AiNewsLoadingScreen
 import com.d104.pnt.ui.home.HomeScreen
 import com.d104.pnt.ui.profile.ProfileScreen
 
@@ -52,7 +53,8 @@ fun MainScreen(userName: String) {
             }
 
             composable(Routes.PROFILE) {
-                ProfileScreen()
+//                ProfileScreen()
+                AiNewsLoadingScreen()
             }
 
             composable(Routes.CHAT) {
