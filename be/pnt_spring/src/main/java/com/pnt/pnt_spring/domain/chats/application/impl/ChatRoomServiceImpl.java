ad29a0d23be58b563807e1969d7e45535c3734c5
권기범 <xtcc02332@naver.com>
2026-01-26 13:03:29 +0900

    [BE: fix] avatar url default 적용

--- be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/chats/application/impl/ChatRoomServiceImpl.java
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/chats/application/impl/ChatRoomServiceImpl.java
@@ -52,13 +52,6 @@ public class ChatRoomServiceImpl implements ChatRoomService {
                         .build()
                 );
 
-        // 기존 row가 있었던 경우(soft delete 됐던 경우 포함) 복구 + 연결상태 정리
-//        if (mcr.isDeleted()) {
-//            mcr.rejoin();     // isDeleted=false, isConnected=true
-//        } else {
-//            mcr.connect();    // isConnected=true, isDeleted=false
-//        }
-
         memberChatRoomRepository.save(mcr);
 
         // 4) currentMembers 정합성: 방장은 생성 즉시 1명
