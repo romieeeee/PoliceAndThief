38387890541383984d6e29da0d6d52acfa5f12a3
민승환 <minccc42710@gmail.com>
2026-01-27 08:54:35 +0900

    [BE : feat] 유저 신고 API 작성

---
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/members/report/api/controller/ReportController.java
@@ -0,0 +1,24 @@
+package com.pnt.pnt_spring.domain.members.report.api.controller;
+
+import com.pnt.pnt_spring.domain.members.report.api.req.ReportRequest;
+import com.pnt.pnt_spring.domain.members.report.api.resp.ReportResponse;
+import com.pnt.pnt_spring.domain.members.report.application.ReportServiceImpl;
+import com.pnt.pnt_spring.domain.chats.utils.SecurityUtils;
+import com.pnt.pnt_spring.global.api.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reports")
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportServiceImpl reportService;
+
+    @PostMapping
+    public CommonResponse<ReportResponse> report(@RequestBody ReportRequest request) {
+        Long memberId = SecurityUtils.currentMemberId();
+        ReportResponse response = reportService.createReport(memberId, request);
+        return new CommonResponse<>(response, "신고 접수 완료", HttpStatus.CREATED);
+    }
+}
