70e25e00370244575a56d8c8d75846455d4c3449
이승호 <ssho1204@naver.com>
2026-01-28 15:20:35 +0900

    [FE: feat]: 채팅방 생성 API 연동은 완료 본문: 채팅방 요청은 정상적인 경로로 가는걸 확인, 토큰 만료 문제로 인해 Response는 받지 못했지만 경로는 정상적, 하지만 좌표를 행정구역으로 변환하는거는 아직 안되어서 1110000으로 하드코딩해놓음 푸터: https://ssafy.atlassian.net/browse/S14P11D104-134

--- fe/D104/app/src/main/java/com/d104/pnt/data/remote/api/ChatApiService.kt
+++ fe/D104/app/src/main/java/com/d104/pnt/data/remote/api/ChatApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ChatApiService {
-    @POST("/chats")
+    @POST("chats")
     suspend fun createChatRoom(
         @Body request: ChatCreateRequest
     ): Response<BaseResponse<ChatCreateResponse>>
