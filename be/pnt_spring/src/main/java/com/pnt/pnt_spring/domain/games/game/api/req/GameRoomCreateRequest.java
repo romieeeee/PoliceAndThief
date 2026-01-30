f0dcf3d8f0d959882c8cd6616cdb96cd2887fc0c
minccc42710 <minccc42710@gmail.com>
2026-01-30 15:28:05 +0900

    chore : solve conflict

--- be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/games/game/api/req/GameRoomCreateRequest.java
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/games/game/api/req/GameRoomCreateRequest.java
@@ -32,12 +32,12 @@ public class GameRoomCreateRequest {
 	private Integer cctvInterval;
 
 	@NotNull
-	@Valid
-	private Prison prison;
-
-	@NotNull
 	@Min(0)
 	private Integer missionCount;
+
+	@NotNull
+	@Valid
+	private Prison prison;
 
 	@NotNull
 	@Size(min = 3, message = "polygon은 최소 3개 좌표가 필요합니다.")
