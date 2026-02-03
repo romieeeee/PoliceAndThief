826cd1225c2cdb368736c3d968e160da5e80ead7
이승호 <ssho1204@naver.com>
2026-02-03 17:35:11 +0900

    [FE: fix, refactor, feat] 인게임 지도에서 여러 사람 위치 제공 기능 추가, 코드 세션 레포로 리펙토링, 로직 수정

--- fe/D104/app/src/main/java/com/d104/pnt/MainActivity.kt
+++ fe/D104/app/src/main/java/com/d104/pnt/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.d104.pnt.navigation.AppNavigation
 import com.d104.pnt.ui.theme.D104Theme
+import com.google.android.gms.maps.MapsInitializer
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -23,5 +25,18 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        Thread {
+            try {
+                // Context, 렌더러 옵션(LATEST 추천), 콜백
+                MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST) { renderer ->
+                    when (renderer) {
+                        MapsInitializer.Renderer.LATEST -> Timber.d("구글맵 최신 렌더러 초기화 완료")
+                        MapsInitializer.Renderer.LEGACY -> Timber.d("구글맵 레거시 렌더러 초기화 완료")
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
     }
 }
--- fe/D104/app/src/main/java/com/d104/pnt/data/repository/GameSessionRepository.kt
+++ fe/D104/app/src/main/java/com/d104/pnt/data/repository/GameSessionRepository.kt
@@ -1,31 +1,58 @@
 package com.d104.pnt.data.repository
 
+import com.d104.pnt.data.remote.model.response.BeepUseResponse
 import com.d104.pnt.data.remote.model.response.GameMemberSocketDto
+import com.d104.pnt.data.remote.model.response.MemberLocationSocketDto
 import com.d104.pnt.data.remote.model.response.MissionSocketDto
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.io.File
 
 interface GameSessionRepository {
     val members: StateFlow<List<GameMemberSocketDto>>
     val gameStatus: StateFlow<String>
     val gameId: StateFlow<Long>
+    val gameTime: StateFlow<Int>
     val missions: StateFlow<List<MissionSocketDto>>
+    val memberLocation: StateFlow<List<MemberLocationSocketDto>>
     val eventFlow: SharedFlow<GameSessionEvent>
     val isOutOfBoundary: StateFlow<Boolean>
     val myMemberId: StateFlow<Long>
     val myRole: StateFlow<String>
+    val thiefMembers: StateFlow<List<GameMemberSocketDto>>
+    val escapeQueue: StateFlow<List<String>>
+    val beepEvent: SharedFlow<BeepUseResponse>
+    val longestSurvivalTime: StateFlow<Int>
+    val skillUsedAt: StateFlow<Int?>
+    val survivalTime: StateFlow<Int>
+    val missionState: StateFlow<MissionStatus>
+    val missionFailReason: StateFlow<String>
     val roomCode: StateFlow<String>
-
     val chiefMemberId: StateFlow<Long?>
+    val isChief: StateFlow<Boolean>
+    val helicopterButtonEnabled: StateFlow<Boolean>
+    val helicopterUsed: StateFlow<Boolean>
+    val helicopterState: StateFlow<HelicopterPhase>
+    val walkieState: StateFlow<WalkieConnectionState>
+    val isSomeoneTalking: StateFlow<Boolean>
+    val talkingMemberId: StateFlow<Long?>
+    val isTransmitting: StateFlow<Boolean>
 
     fun setMemberId(memberId: Long)
     fun setFinalRole(role: String)
     fun setRoomCode(code: String)
     fun connectAndJoin(gameId: Long)
     fun gameInit()
+    fun uploadMissionImage(image: File, missionId: Long)
+    fun missionInit()
+    fun dequeEscape()
     fun startGameSession()
     fun stopGameSession()
     fun leaveGame()
-
     fun setChiefMemberId(id: Long?)
+    fun useHelicopterSkill()
+    fun connectWalkie()
+    fun disconnectWalkie()
+    fun startTalking()
+    fun stopTalking()
 }
