a249742fdc0a1b37494c9103458cd98a028dac67
권기범 <xtcc02332@naver.com>
2026-02-03 14:20:07 +0900

    [BE:feat] fcm 토큰 등록 및 활성화 비활성화 API 구현

---
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/members/member/entity/FcmToken.java
@@ -0,0 +1,50 @@
+package com.pnt.pnt_spring.domain.members.member.entity;
+
+import com.pnt.pnt_spring.domain.utils.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "fcm_token")
+public class FcmToken extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JoinColumn(name = "member_id")
+	@OneToOne
+	private Member member;
+
+	@Column(name = "value", nullable = false)
+	private String value;
+
+	@Column(name = "is_active", nullable = false)
+	@Builder.Default
+	private boolean isActive = false;
+
+	@Column(name = "is_deleted", nullable = false)
+	@Builder.Default
+	private boolean isDeleted = false;
+
+	// active 업데이트
+	public void updateActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+}
