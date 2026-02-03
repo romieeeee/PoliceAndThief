03ea56ec96e68d315b51d353c5404f22614f9365
권기범 <xtcc02332@naver.com>
2026-02-03 16:50:36 +0900

    [BE:feat] fcm 로깅 추가

--- be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/members/member/entity/FcmToken.java
+++ be/pnt_spring/src/main/java/com/pnt/pnt_spring/domain/members/member/entity/FcmToken.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
