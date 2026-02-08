99ed3236f4d11684f298265c5a6ace816279ce2d
권기범 <xtcc02332@naver.com>
2026-02-09 00:37:15 +0900

    [docs] dump 파일 추가

---
+++ exec/d104_dump/postgresql/spatial_ref_sys-dump.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict KP0tKdtaOckBWXgzxCTvuUWK7ExtGb3tKFE4f2SmHXNahmTIAxQVtSDJaOasqt9
+
+-- Dumped from database version 15.4 (Debian 15.4-1.pgdg110+1)
+-- Dumped by pg_dump version 18.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: spatial_ref_sys; Type: TABLE DATA; Schema: public; Owner: root
+--
+
+COPY public.spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict KP0tKdtaOckBWXgzxCTvuUWK7ExtGb3tKFE4f2SmHXNahmTIAxQVtSDJaOasqt9
+
